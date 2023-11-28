a5841a3027628f8fb89b604d8aa4b863e8925692
Alex <abdiep@ualberta.ca>
2023-11-28 05:27:40 -0700

    asd

--- Lab 7/matplotlib/plot.py
+++ Lab 7/matplotlib/plot.py
@@ -1,29 +1,32 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from scipy.optimize import curve_fit
 
-def linear(x, m,):
-    return m * x
+df = pd.read_csv(r'Lab 7\matplotlib\hot_wire.csv')
+x = df['pressure']
+y = df['voltage']
 
+plt.plot(x, y, 'ko', markersize=6, markerfacecolor='none')
+# linear line
+m, b = np.polyfit(x, y, 1)
+plt.plot(x, m*x + b, 'k--')
+plt.xlabel('$P_v$, Dynamic pressure (mm$H_2$O)')
+plt.ylabel('$V$, Anemometer voltage (V)')
+plt.legend(['Data', 'Linear fit'])
+plt.text(5, 8, f'$V = {m:.3f}P_v + {b:.3f}$')
+plt.savefig(r'Lab 7\matplotlib\anemometer_calibration.png', dpi=300)
 
-# Question 2
-# plot the function y= 0.8*3.3/(2)*(x+1) +0.1*3.3
-# % 2) See Page 6 of the data sheet. The output of the sensor depends on the supply 
-# % voltage of the sensor. Plot the expected voltage output of your sensor as a 
-# % function of pressure over the range of the sensor if the supply voltage is 3.3 V 
-# % and if it is 5V. Put both lines on the same plot. [2 marks]. Calculate the 
-# % sensitivity of the sensor for each supply voltage. [1 mark]
-x = np.linspace(1, -1, 100)
-y_33 = 0.8*3.3/(2)*(x+1) +0.1*3.3
-y_5 = 0.8*5/(2)*(x+1) +0.1*5
-plt.plot(x, y_33, 'k--', label='Supply Voltage = 3.3V')
-plt.plot(x, y_5, 'k-.', label='Supply Voltage = 5V')
-plt.xlabel('$P$, Applied Pressure (kPa)')
-plt.ylabel('$V_{3.3}, V_{5.0}$, Output Voltage (V)')
-plt.text(0, 1, '$V_{3.3} = 1.32*(P+1) + 0.33$')
-plt.text(0, 4, '$V_{5.0} = 2*(P+1) + 0.5$')
-plt.legend()
+df_turbine = pd.read_csv(r'Lab 7\matplotlib\turbine.csv')
+x_turbine = df_turbine['Q'] # m^3/s
+y_turbine = df_turbine['turbine'] # hz
+m_turbine, b_turbine = np.polyfit(x_turbine, y_turbine, 1)
+plt.figure()
+plt.plot(x_turbine, y_turbine, 'ko', markersize=6, markerfacecolor='none')
+plt.plot(x_turbine, m_turbine*x_turbine + b_turbine, 'k--')
+plt.xlabel('$Q$, Flow rate (m$^3$/s)')
+plt.ylabel('$f$, Turbine frequency (Hz)')
+plt.legend(['Data', 'Linear fit'])
+plt.text(0.02, 200, f'$f = {m_turbine:.3f}Q + {b_turbine:.3f}$')
+plt.savefig(r'Lab 7\matplotlib\turbine_calibration.png', dpi=300)
 
-#save
-plt.savefig(r'Lab 5/matplotlib/q2VoltageOutputPlot.png', dpi = 300)
+plt.show()
